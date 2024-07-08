5632f88347dec64b1a43feb8030ce00c11624bd8
ARJUN AJITH <arjun@ARJUNs-MacBook-Pro.local>
2024-07-09 01:01:58 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AudioRecorder from './components/Landing'
+
+function App() {
+
+  return (
+    <>
+
+    
+    <AudioRecorder />
+      
+    </>
+  )
+}
+
+export default App
