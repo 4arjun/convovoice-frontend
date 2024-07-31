86622a68eadad67646bfe9aa850655bfc03a6268
Arjun <arjunajith440@gmail.com>
2024-07-31 22:54:54 +0530

    added pricing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -15,10 +15,17 @@ const Home = () => {
     <div className='main-section'>
       <div>
         <h1>Welcome to ConvoVoice...</h1>
-        <p>The tutor you needed is just a click away! </p>
+        <p>The tutor you needed is just a click away!</p>
         <p>Give it a try</p>
       </div>
       <button className='btn-processing' onClick={handleClick}>go to processing</button>
+      <div className='pricing-container'>
+        <div>
+          <h2>Free trial(new users)</h2>
+          <p>1Month </p>
+          <p>5 requests per day</p>
+        </div>
+      </div>
     </div>
   );
 };
