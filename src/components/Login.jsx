7996f9efb39cc967a57f5667d2efdd6f96cd805e
Arjun <arjunajith440@gmail.com>
2024-09-07 16:51:33 +0530

    added preloader

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import "./Login.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import bgimg from "../assets/bg-img.jpg";
 import { useNavigate } from "react-router-dom";
 import { Alert } from "@mui/material";
@@ -10,6 +10,7 @@ const Login2 = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isLoading, setIsLoader] = useState(true);
 
   const [showEmailPlaceholder, setShowEmailPlaceholder] = useState(false);
   const [showPasswordPlaceholder, setShowPasswordPlaceholder] = useState(false);
@@ -19,6 +20,10 @@ const Login2 = () => {
 
   const handlePasswordFocus = () => setShowPasswordPlaceholder(true);
   const handlePasswordBlur = () => setShowPasswordPlaceholder(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoader(false), 3000);
+  }, []);
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -58,6 +63,7 @@ const Login2 = () => {
     <div className="login-container">
       {showAlert && (
         <Alert
+          className="alert-container"
           style={{
             zIndex: "1",
             position: "absolute",
@@ -75,188 +81,192 @@ const Login2 = () => {
         </Alert>
       )}
 
-      <div className="bg-img-container">
-        <img src={bgimg} alt="" />
-        <div className="login-form">
-          <div className="login-items">
-            <h2 className="login-text">Login</h2>
-            <div
-              style={{
-                textAlign: "left",
-                width: "80%",
-                display: "flex",
-                flexDirection: "column",
-                gap: "20px",
-              }}
-            >
-              <div style={{ display: "flex", flexDirection: "row" }}>
-                <svg
-                  style={{ position: "absolute" }}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 24 24"
+      {isLoading ? (
+        <div id="preloader"></div>
+      ) : (
+        <div className="bg-img-container">
+          <img src={bgimg} alt="" />
+          <div className="login-form">
+            <div className="login-items">
+              <h2 className="login-text">Login</h2>
+              <div
+                style={{
+                  textAlign: "left",
+                  width: "80%",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "20px",
+                }}
+              >
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <svg
+                    style={{ position: "absolute" }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 24 24"
+                  >
+                    <g
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-dasharray="64"
+                        stroke-dashoffset="64"
+                        d="M4 5h16c0.55 0 1 0.45 1 1v12c0 0.55 -0.45 1 -1 1h-16c-0.55 0 -1 -0.45 -1 -1v-12c0 -0.55 0.45 -1 1 -1Z"
+                      >
+                        <animate
+                          fill="freeze"
+                          attributeName="stroke-dashoffset"
+                          dur="0.6s"
+                          values="64;0"
+                        />
+                      </path>
+                      <path
+                        stroke-dasharray="24"
+                        stroke-dashoffset="24"
+                        d="M3 6.5l9 5.5l9 -5.5"
+                      >
+                        <animate
+                          fill="freeze"
+                          attributeName="stroke-dashoffset"
+                          begin="0.6s"
+                          dur="0.2s"
+                          values="24;0"
+                        />
+                      </path>
+                    </g>
+                  </svg>
+
+                  <input
+                    onFocus={handleEmailFocus}
+                    onBlur={handleEmailBlur}
+                    className={
+                      showEmailPlaceholder
+                        ? "hideemailplaceholder"
+                        : "email-placeholder"
+                    }
+                    style={{
+                      backgroundColor: "transparent",
+                      border: "None",
+                      borderBottom: "1px solid #ffffff",
+                      width: "100%",
+                      color: "white",
+                      outline: "None",
+                      paddingLeft: "20px",
+                    }}
+                    placeholder="USERNAME/EMAIL"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    type="email"
+                    id="email"
+                    name="email"
+                    required
+                  />
+                </div>
+
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    paddingTop: "6px",
+                  }}
                 >
-                  <g
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                  <svg
+                    style={{ position: "absolute" }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 24 24"
                   >
                     <path
-                      stroke-dasharray="64"
-                      stroke-dashoffset="64"
-                      d="M4 5h16c0.55 0 1 0.45 1 1v12c0 0.55 -0.45 1 -1 1h-16c-0.55 0 -1 -0.45 -1 -1v-12c0 -0.55 0.45 -1 1 -1Z"
-                    >
-                      <animate
-                        fill="freeze"
-                        attributeName="stroke-dashoffset"
-                        dur="0.6s"
-                        values="64;0"
-                      />
-                    </path>
-                    <path
-                      stroke-dasharray="24"
-                      stroke-dashoffset="24"
-                      d="M3 6.5l9 5.5l9 -5.5"
-                    >
-                      <animate
-                        fill="freeze"
-                        attributeName="stroke-dashoffset"
-                        begin="0.6s"
-                        dur="0.2s"
-                        values="24;0"
-                      />
-                    </path>
-                  </g>
-                </svg>
-
-                <input
-                  onFocus={handleEmailFocus}
-                  onBlur={handleEmailBlur}
-                  className={
-                    showEmailPlaceholder
-                      ? "hideemailplaceholder"
-                      : "email-placeholder"
-                  }
-                  style={{
-                    backgroundColor: "transparent",
-                    border: "None",
-                    borderBottom: "1px solid #ffffff",
-                    width: "100%",
-                    color: "white",
-                    outline: "None",
-                    paddingLeft: "20px",
-                  }}
-                  placeholder="EMAIL"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  type="email"
-                  id="email"
-                  name="email"
-                  required
-                />
+                      fill="currentColor"
+                      d="M12 17a2 2 0 0 1-2-2c0-1.11.89-2 2-2a2 2 0 0 1 2 2a2 2 0 0 1-2 2m6 3V10H6v10zm0-12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10c0-1.11.89-2 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2zm-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3"
+                    />
+                  </svg>
+                  <input
+                    onFocus={handlePasswordFocus}
+                    onBlur={handlePasswordBlur}
+                    className={
+                      showPasswordPlaceholder
+                        ? "hidepasswordplaceholder"
+                        : "password-placeholder"
+                    }
+                    style={{
+                      backgroundColor: "transparent",
+                      border: "None",
+                      borderBottom: "1px solid #ffffff",
+                      width: "100%",
+                      outline: "None",
+                      color: "white",
+                      paddingLeft: "20px",
+                    }}
+                    placeholder="PASSWORD"
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    name="password"
+                    required
+                  />
+                </div>
               </div>
 
               <div
                 style={{
                   display: "flex",
                   flexDirection: "row",
-                  paddingTop: "6px",
+                  paddingTop: "15px",
+                  justifyContent: "space-between",
+                  width: "80%",
                 }}
               >
-                <svg
-                  style={{ position: "absolute" }}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M12 17a2 2 0 0 1-2-2c0-1.11.89-2 2-2a2 2 0 0 1 2 2a2 2 0 0 1-2 2m6 3V10H6v10zm0-12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10c0-1.11.89-2 2-2h1V6a5 5 0 0 1 5-5a5 5 0 0 1 5 5v2zm-6-5a3 3 0 0 0-3 3v2h6V6a3 3 0 0 0-3-3"
-                  />
-                </svg>
                 <input
-                  onFocus={handlePasswordFocus}
-                  onBlur={handlePasswordBlur}
-                  className={
-                    showPasswordPlaceholder
-                      ? "hidepasswordplaceholder"
-                      : "password-placeholder"
-                  }
-                  style={{
-                    backgroundColor: "transparent",
-                    border: "None",
-                    borderBottom: "1px solid #ffffff",
-                    width: "100%",
-                    outline: "None",
-                    color: "white",
-                    paddingLeft: "20px",
-                  }}
-                  placeholder="PASSWORD"
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  name="password"
-                  required
+                  style={{ marginTop: "3px" }}
+                  type="checkbox"
+                  id="myCheckbox"
+                  name="myCheckbox"
+                  value="value1"
                 />
+                <p style={{ marginLeft: "-55px", fontSize: "16px" }}>
+                  Remember me
+                </p>
+                <a className="forgot-password-text" href="#">
+                  Forgot Password
+                </a>
               </div>
-            </div>
 
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                paddingTop: "15px",
-                justifyContent: "space-between",
-                width: "80%",
-              }}
-            >
-              <input
-                style={{ marginTop: "3px" }}
-                type="checkbox"
-                id="myCheckbox"
-                name="myCheckbox"
-                value="value1"
-              />
-              <p style={{ marginLeft: "-55px", fontSize: "16px" }}>
-                Remember me
-              </p>
-              <a className="forgot-password-text" href="#">
-                Forgot Password
-              </a>
-            </div>
+              <button
+                onClick={handleLogin}
+                style={{ marginTop: "15px" }}
+                className="login-btn"
+              >
+                Login
+              </button>
 
-            <button
-              onClick={handleLogin}
-              style={{ marginTop: "15px" }}
-              className="login-btn"
-            >
-              Login
-            </button>
-
-            <div
-              style={{
-                paddingTop: "5px",
-                display: "flex",
-                flexDirection: "row",
-                width: "80%",
-                justifyContent: "center",
-              }}
-            >
-              <p style={{ fontWeight: "200", fontSize: "15px" }}>
-                Dont have an account?
-              </p>
-              <a className="register-text" href="#">
-                Register
-              </a>
+              <div
+                style={{
+                  paddingTop: "5px",
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "80%",
+                  justifyContent: "center",
+                }}
+              >
+                <p style={{ fontWeight: "200", fontSize: "15px" }}>
+                  Dont have an account?
+                </p>
+                <a className="register-text" href="#">
+                  Register
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
